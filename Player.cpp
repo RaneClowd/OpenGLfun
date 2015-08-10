794d7f541ed5a798d2f0e13c908010fe16009ee3
raneclowd <skaggskd88@gmail.com>
2015-08-09 22:04:22 -0500

    refactoring player position management

--- Player.cpp
+++ Player.cpp
@@ -5,7 +5,8 @@ const int INITIAL_HEIGHT = 2;
 const float MOVE_SPEED = .01f, LOOK_SPEED = .01f, GRAVITY_FORCE = -.00005;
 
 unsigned char walkDirection = NONE;
-glm::vec3 worldShift = glm::vec3(0, -INITIAL_HEIGHT, -4); // "It came to me in a dream. The engines don't move the ship at all. The ship stays where it is, and the engines move the universe around it." -Professor Cubert J. Farnsworth
+//glm::vec3 worldShift = glm::vec3(0, -INITIAL_HEIGHT, -4); // "It came to me in a dream. The engines don't move the ship at all. The ship stays where it is, and the engines move the universe around it." -Professor Cubert J. Farnsworth
+glm::vec3 playerLocation = glm::vec3(0, INITIAL_HEIGHT, 4);
 
 glm::vec3 angle = glm::vec3(0, -0.5, 0);
 
@@ -22,37 +23,38 @@ void initPlayerInput(void) {
 
 glm::mat4 updatePlayerView(Uint32 msLapsed) {
     if (walkDirection & FORWARD) {
-        worldShift.z += cosf(angle.x) * MOVE_SPEED * msLapsed;
-        worldShift.x -= sinf(angle.x) * MOVE_SPEED * msLapsed;
+        playerLocation.z -= cosf(angle.x) * MOVE_SPEED * msLapsed;
+        playerLocation.x += sinf(angle.x) * MOVE_SPEED * msLapsed;
     }
     if (walkDirection & BACKWARD) {
-        worldShift.z -= cosf(angle.x) * MOVE_SPEED * msLapsed;
-        worldShift.x += sinf(angle.x) * MOVE_SPEED * msLapsed;
+        playerLocation.z += cosf(angle.x) * MOVE_SPEED * msLapsed;
+        playerLocation.x -= sinf(angle.x) * MOVE_SPEED * msLapsed;
     }
 
     if (walkDirection & LEFT) {
-        worldShift.x += cosf(angle.x) * MOVE_SPEED * msLapsed;
-        worldShift.z += sinf(angle.x) * MOVE_SPEED * msLapsed;
+        playerLocation.x -= cosf(angle.x) * MOVE_SPEED * msLapsed;
+        playerLocation.z -= sinf(angle.x) * MOVE_SPEED * msLapsed;
     }
     if (walkDirection & RIGHT) {
-        worldShift.x -= cosf(angle.x) * MOVE_SPEED * msLapsed;
-        worldShift.z -= sinf(angle.x) * MOVE_SPEED * msLapsed;
+        playerLocation.x += cosf(angle.x) * MOVE_SPEED * msLapsed;
+        playerLocation.z += sinf(angle.x) * MOVE_SPEED * msLapsed;
     }
 
-    if (msLapsed != 0 && (jumpVelocity != 0 || worldShift.y != 0)) {
-        worldShift.y -= jumpVelocity * msLapsed;
+    if (msLapsed != 0 && (jumpVelocity != 0 || playerLocation.y != INITIAL_HEIGHT)) {
+        playerLocation.y += jumpVelocity * msLapsed;
 
         jumpVelocity += GRAVITY_FORCE * msLapsed;
 
-        if (worldShift.y > -INITIAL_HEIGHT) {
-            worldShift.y = -INITIAL_HEIGHT; // at rest, the world is pushed down by how tall you are. because zero is defined by where your eyes are
+        if (playerLocation.y < INITIAL_HEIGHT) {
+            playerLocation.y = INITIAL_HEIGHT;
             jumpVelocity = 0;
         }
     }
 
     glm::mat4 view = glm::rotate(IDENTITY_MATRIX, angle.y, glm::vec3(-1, 0, 0));
     view = glm::rotate(view, angle.x, glm::vec3(0, 1, 0));
-    view = glm::translate(view, worldShift);
+
+    view = glm::translate(view, -playerLocation); // "It came to me in a dream. The engines don't move the ship at all. The ship stays where it is, and the engines move the universe around it." -Professor Cubert J. Farnsworth
 
     return view;
 }
@@ -82,7 +84,7 @@ void checkForPlayerInput(void) {
                     break;
                 }
                 case SDL_SCANCODE_SPACE: {
-                    if (worldShift.y == -INITIAL_HEIGHT) {
+                    if (playerLocation.y == INITIAL_HEIGHT) {
                         jumpVelocity = .02;
                         jumpDetected = 1;
                     }
